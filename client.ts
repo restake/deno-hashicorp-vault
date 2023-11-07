f5ff03d96f4379bf1d703188b1cef9432131c479
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-11-07 23:44:33 +0200

    Add support for AbortSignal for requests

--- client.ts
+++ client.ts
@@ -8,6 +8,7 @@ import { doVaultFetch } from "./vault.ts";
 export type VaultRequestOptions = {
     method?: string;
     wrapTTL?: number | string;
+    signal?: AbortSignal;
 };
 
 export class VaultClient<T extends VaultAuthentication> {
@@ -31,7 +32,7 @@ export class VaultClient<T extends VaultAuthentication> {
         return this.currentTokenAccessor!;
     }
 
-    async login(): Promise<void> {
+    async login(opts?: Pick<VaultRequestOptions, "signal">): Promise<void> {
         const authType = this.credentials.authentication[VAULT_AUTH_TYPE];
 
         this.currentLeaseDuration = 0;
@@ -39,7 +40,7 @@ export class VaultClient<T extends VaultAuthentication> {
 
         switch (authType) {
             case "approle": {
-                const { client_token, accessor, lease_duration, renewable } = await this.approleLogin();
+                const { client_token, accessor, lease_duration, renewable } = await this.approleLogin(opts);
                 this.currentToken = client_token;
                 this.currentTokenAccessor = accessor;
 
@@ -51,7 +52,7 @@ export class VaultClient<T extends VaultAuthentication> {
                 const tokenCredentials = this.credentials.authentication as unknown as VaultTokenCredentials;
                 this.currentToken = tokenCredentials.token;
 
-                const { data: { accessor, renewable, ttl } } = await this.lookup();
+                const { data: { accessor, renewable, ttl } } = await this.lookup(undefined, opts);
                 this.currentTokenAccessor = accessor;
 
                 this.currentLeaseDuration = ttl;
@@ -75,7 +76,9 @@ export class VaultClient<T extends VaultAuthentication> {
         }
     }
 
-    async approleLogin(): Promise<{ client_token: string; accessor: string; lease_duration: number; renewable: boolean }> {
+    async approleLogin(
+        opts?: Pick<VaultRequestOptions, "signal">,
+    ): Promise<{ client_token: string; accessor: string; lease_duration: number; renewable: boolean }> {
         const authType = this.credentials.authentication[VAULT_AUTH_TYPE];
         if (authType !== "approle") {
             throw new Error(`approleLogin cannot be used with authentication type "${authType}"`);
@@ -88,7 +91,7 @@ export class VaultClient<T extends VaultAuthentication> {
         const res = await doVaultFetch(LoginResponse, address, namespace, undefined, `${mountpoint}/login`, { method: "POST" }, {
             role_id: roleID,
             secret_id: secretID,
-        });
+        }, opts?.signal);
 
         return {
             client_token: res.auth.client_token,
@@ -98,7 +101,10 @@ export class VaultClient<T extends VaultAuthentication> {
         };
     }
 
-    async lookup(accessor?: string): Promise<TokenLookupResponse> {
+    async lookup(
+        accessor?: string,
+        opts?: Pick<VaultRequestOptions, "signal">,
+    ): Promise<TokenLookupResponse> {
         this.assertToken();
 
         const { address, namespace } = this.credentials;
@@ -110,12 +116,16 @@ export class VaultClient<T extends VaultAuthentication> {
             `auth/token/lookup` + (accessor ? "-accessor" : "-self"),
             { method: accessor ? "POST" : "GET" },
             accessor ? { accessor } : undefined,
+            opts?.signal,
         );
 
         return res;
     }
 
-    async renewToken(accessor?: string): Promise<{ accessor: string; lease_duration: number }> {
+    async renewToken(
+        accessor?: string,
+        opts?: Pick<VaultRequestOptions, "signal">,
+    ): Promise<{ accessor: string; lease_duration: number }> {
         this.assertToken();
 
         const { address, namespace } = this.credentials;
@@ -128,6 +138,7 @@ export class VaultClient<T extends VaultAuthentication> {
             "auth/token/renew" + (accessor ? "-accessor" : "-self"),
             { method: "POST" },
             accessor ? { accessor } : undefined,
+            opts?.signal,
         );
 
         return {
@@ -138,15 +149,25 @@ export class VaultClient<T extends VaultAuthentication> {
 
     async issueToken(
         role?: string,
+        opts?: Pick<VaultRequestOptions, "signal">,
     ): Promise<{ client_token: string; accessor: string; lease_duration: number }> {
         this.assertToken();
 
         const { address, namespace } = this.credentials;
 
         const endpoint = `auth/token/create` + (role ? `/${role}` : "");
-        const res = await doVaultFetch(LoginResponse, address, namespace, this.currentToken, endpoint, {
-            method: "POST",
-        });
+        const res = await doVaultFetch(
+            LoginResponse,
+            address,
+            namespace,
+            this.currentToken,
+            endpoint,
+            {
+                method: "POST",
+            },
+            undefined,
+            opts?.signal,
+        );
 
         return {
             client_token: res.auth.client_token,
@@ -158,7 +179,11 @@ export class VaultClient<T extends VaultAuthentication> {
     async read<
         T extends ZodType,
         R = z.output<T>,
-    >(type: T, endpoint: string, opts?: VaultRequestOptions): Promise<R> {
+    >(
+        type: T,
+        endpoint: string,
+        opts?: VaultRequestOptions,
+    ): Promise<R> {
         this.assertToken();
 
         const { address, namespace } = this.credentials;
@@ -175,13 +200,20 @@ export class VaultClient<T extends VaultAuthentication> {
                     ...(opts?.wrapTTL && { "x-vault-wrap-ttl": `${opts.wrapTTL}` }),
                 },
             },
+            undefined,
+            opts?.signal,
         );
     }
 
     async write<
         T extends ZodType | undefined,
         R extends (T extends ZodType ? z.output<T> : undefined),
-    >(type: T, endpoint: string, body: unknown, opts?: VaultRequestOptions): Promise<R> {
+    >(
+        type: T,
+        endpoint: string,
+        body: unknown,
+        opts?: VaultRequestOptions,
+    ): Promise<R> {
         this.assertToken();
 
         const { address, namespace } = this.credentials;
@@ -199,13 +231,19 @@ export class VaultClient<T extends VaultAuthentication> {
                 },
             },
             body,
+            opts?.signal,
         );
     }
 
     async unwrap<
         T extends ZodType,
         R = z.output<T>,
-    >(type: T, wrappingToken: string, expectedCreationPath?: string): Promise<R> {
+    >(
+        type: T,
+        wrappingToken: string,
+        expectedCreationPath?: string,
+        opts?: Pick<VaultRequestOptions, "signal">,
+    ): Promise<R> {
         const { address, namespace } = this.credentials;
 
         // Check creation path, if specified
@@ -222,6 +260,7 @@ export class VaultClient<T extends VaultAuthentication> {
                 {
                     token: wrappingToken,
                 },
+                opts?.signal,
             );
 
             if (expectedCreationPath !== creationData.creation_path) {
@@ -238,6 +277,8 @@ export class VaultClient<T extends VaultAuthentication> {
             {
                 method: "POST",
             },
+            undefined,
+            opts?.signal,
         );
     }
 
